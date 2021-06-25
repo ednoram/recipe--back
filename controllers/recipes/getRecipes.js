a10b177b4077ac424f8f03caa0a5a89cefdf6dd8
ednoram <e.noramirian@gmail.com>
2021-06-25 19:33:59 +0400

    add email query param to get recipes

--- controllers/recipes/getRecipes.js
+++ controllers/recipes/getRecipes.js
@@ -1,11 +1,15 @@
 const { Recipe } = require("../../models");
 
-const getRecipes = (_req, res) => {
-  Recipe.find()
-    .then((data) => res.status(200).json(data))
-    .catch((err) =>
-      res.status(404).json({ errors: [{ message: err.message }] })
-    );
+const getRecipes = (req, res) => {
+  try {
+    const { email } = req.query;
+
+    email
+      ? Recipe.find({ email }).then((data) => res.status(200).json(data))
+      : Recipe.find().then((data) => res.status(200).json(data));
+  } catch (err) {
+    res.status(404).json({ errors: [{ message: err.message }] });
+  }
 };
 
 module.exports = getRecipes;
