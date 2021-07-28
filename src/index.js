c677040d9a834f9d728a7798540c4570b49181c5
ednoram <e.noramirian@gmail.com>
2021-07-28 13:18:24 +0400

    main

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-const cookieParser = require("cookie-parser");
 require("dotenv").config();
 
 const {
@@ -16,11 +15,8 @@ const { connectDB } = require("./config");
 const app = express();
 
 app.use(express.json());
-app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({ origin: true, credentials: true }));
-
-app.use("/api/public/uploads", express.static("public/uploads"));
 
 app.use("/api/user", userRoutes);
 app.use("/api/uploads", uploadsRoutes);
